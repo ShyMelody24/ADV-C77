670b2c23ac6c960199c98eb7593a62afae126a3c
ShyMelody24 <77199644+ShyMelody24@users.noreply.github.com>
2021-07-01 19:50:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("My first EVER console Project in WHJR!")
+var first_name = "Sanjana";
+var last_name = "Kolavennu";
+var full_name= first_name.concat(last_name);
+console.log(full_name)
+
+var a = 10;
+var b = 2;
+var add = a + b;
+console.log(add);
+
+var sub = b - a;
+console.log(sub);
+
+var multiply = b * a;
+console.log(multiply);
+
+var divide = a / b;
+console.log(divide);
